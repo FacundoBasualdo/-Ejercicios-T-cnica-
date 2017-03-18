d7427995963e1c36d567a0bcb431c09fb5231cb5
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-18 19:54:11 -0300

    Primer Ejercicio :D

--- Ejercicio_1 .py
+++ Ejercicio_1 .py
@@ -1,26 +1,33 @@
+
 print ("")
-usuario_creado = raw_input("Crea Un Nombre De Usuario: ")
-contrasena_creado = raw_input("Crea Una Contrasena: ")
+usuario_creado = raw_input( "Crea Un Nombre De Usuario: ")
+contrasena_creado = raw_input(" Crea Una Contrasena: ")
 print ("")
-print ("Usted A Creado Un Usuario Y Contrasena Escribala Correctamaente Para Ingresar Al Sistema")
+print ( "Usted A Creado Un Usuario Y Contrasena Escribala Correctamaente Para Ingresar Al Sistema.")
 print ("")
+
 
 usuario_correcto = True
 while (usuario_correcto):
-    usuario = raw_input("Ingrese Su Usuario Correctamente: ")
+    usuario = raw_input(" Ingrese Su Usuario Correctamente: ")
+    print ("")
     if (usuario_creado == usuario):
         usuario_correcto = False
     else:
         print ("")
-        print ("Usuario Incorrecto")
-contrasena_correcta = True
+        print (" Usuario Incorrecto.")
+        print ("")
 
+
+contrasena_correcta = True
 while (contrasena_correcta):
-    contrasena = raw_input("Ingrese Su contrasena Correctamente: ")
+    contrasena = raw_input(" Ingrese Su contrasena Correctamente: ")
+    print ("")
     if (contrasena_creado == contrasena):
         print ("")
-        print ("A Ingresado Al Sistema Correctamente")
+        print (" A Ingresado Al Sistema Correctamente.")
         contrasena_correcta = False
     else:
         print ("")
-        print ("Contrasena Incorrecta")
+        print (" Contrasena Incorrecta.")
+        print ("")
