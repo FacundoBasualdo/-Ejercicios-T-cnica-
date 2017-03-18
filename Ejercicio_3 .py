5463d716379be6cb1e86aac3d746ad57ca61337b
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-18 20:55:41 -0300

    Tercer Ejercicio :D

--- Ejercicio_3 .py
+++ Ejercicio_3 .py
@@ -1,15 +1,21 @@
 print ("")
 monto = raw_input(" Escriba Total A Pagar: $")
 print ("")
+
+
 encendido = True
 while (encendido):
     metodo_pago = raw_input(" Ponga --> 1 <-- Si Paga Al Contado, --> 2 <-- Para No: ")
     print ("")
+
+
     if (metodo_pago == "1"):
         descuento = int(monto) / 10
         total_descuento = int(monto) - int(descuento)
         print (" El Total Es De $" + str(total_descuento))
         encendido = False
+
+
     elif (metodo_pago == "2"):
         print (" $" + monto)
         encendido = False
