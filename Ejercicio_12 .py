9c37ca31fccc8d5f57191cfc49e9bcf5e561300d
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-19 03:58:38 -0300

    Ejercicio 12 :|

---
+++ Ejercicio_12 .py
@@ -0,0 +1,12 @@
+print ("")
+lista_numeros = []
+pedir = True
+contar = 0
+while(pedir):
+    lista_numeros.append(raw_input("Ingrese Un Numero: "))
+    print ("")
+    for numero in lista_numeros:
+        contar = contar + 1
+        if (contar ==  50):
+            print lista_numeros
+            pedir = False
