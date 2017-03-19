e471d8a9fa0d65690a8f51d1932856d95de62868
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-19 06:39:46 -0300

    Ejercicio 16 :C

---
+++ Ejercicio_16 .py
@@ -0,0 +1,19 @@
+lista_numeros = []
+encender = True
+print ("")
+print ("Al Finalizar De Ingresar Escriba (salir)")
+print ("")
+while(encender):
+    lista_numeros.append(raw_input("Escribir Numero: "))
+    print ("")
+    for caracter in lista_numeros:
+        if (caracter == "salir"):
+            lista_numeros.remove("salir")
+            encender = False
+
+numero_menor = lista_numeros[0]
+for numero in lista_numeros:
+    if (numero < numero_menor):
+        numero_menor = numero
+
+print ("El Numero Menor Es " + numero_menor)
