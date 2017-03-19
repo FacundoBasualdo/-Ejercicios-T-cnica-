0f9f50a85fb8207fb5152423bbb28bb4a330c6ff
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-19 03:22:29 -0300

    Ejercicio 10 :|

--- Ejercicio_10 .py
+++ Ejercicio_10 .py
@@ -1,7 +1,4 @@
 contador = 0
-lista_contados = []
-while (contador <= 100):
-    lista_contados.append(contador)
-    contador = contador + 1
-    if (contador == 101):
-        print lista_contados
+while (contador < 40):
+    contador = contador + 2
+    print contador
