957721226d58865c2ab2b6666176bb0099a22844
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-20 12:44:22 -0300

    Delete Ejercicio_10 .py

--- Ejercicio_10 .py
+++
@@ -1,7 +0,0 @@
-contador = 0
-lista_contados = []
-while (contador <= 100):
-    lista_contados.append(contador)
-    contador = contador + 1
-    if (contador == 101):
-        print lista_contados
