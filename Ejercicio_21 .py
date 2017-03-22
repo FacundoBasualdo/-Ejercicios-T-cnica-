9bf5ff6556f047e137d375b74219bae0a7dcc71f
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-21 21:13:12 -0300

    Ejercicio 21 D:

--- Ejercicio_21 .py
+++ Ejercicio_21 .py
@@ -9,9 +9,25 @@ def buscar_reemplazar(lista, buscador, reemplazante):
 
     return lista_meter
 
+lista = []
+crear_lista = True
+while (crear_lista):
+    print ("")
+    lista.append(raw_input("Ingrese Una Lista Con Cualquier Tipo De Datos. Al Finalizar Escribir (salir1): "))
+    for dato in lista:
+        if (dato == "salir1"):
+            lista.remove("salir1")
+            crear_lista = False
 
-lista_creada = [1, 2, 3, 4, 5, 6]
-
-lista_con_cambios = buscar_reemplazar(lista_creada, 2, 9)
 print ("")
-print (lista_con_cambios)
+print (lista)
+print ("")
+buscador = raw_input("Ingrese Un Dato Que Desee Cambiar De Su Lista: ")
+print ("")
+reemplazante = raw_input("Ingrese El Dato Que Quiere Cambiar Por El Anterior: ")
+
+print ("")
+print ("")
+print (buscar_reemplazar(lista, buscador, reemplazante))
+print ("")
+print ("")
