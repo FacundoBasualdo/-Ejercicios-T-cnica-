72e9da78745e7ab7518a91ff5d788ecbfcff69d3
FacundoBasualdo <facubasualdo49@gmail.com>
2017-03-29 17:11:22 -0300

    25 completo

---
+++ ejercicio_25.py
@@ -0,0 +1,9 @@
+print ("")
+frase = raw_input ("Ingrese Una Frase: ")
+print ("")
+cant_frase = len(frase)
+sumar = ""
+lista_parte = []
+for caracter in frase:
+    sumar += caracter
+    print sumar
